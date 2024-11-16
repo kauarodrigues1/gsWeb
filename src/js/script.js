3eef25d76d73bd428753b42b18250ce5c57ea6ee
Kaua <rodrigueskaua76@gmail.com>
2024-11-15 23:14:11 -0300

    pagina inteira trocando de cor

--- src/js/script.js
+++ src/js/script.js
@@ -20,3 +20,39 @@ function sai() {
 elem.addEventListener("mouseenter", entrei);
 //elem.addEventListener("mousemove", mover);
 elemItens.addEventListener("mouseleave", sai);
+
+
+function trocarCorBotao1() {
+    const botao1 = document.getElementById("botao1");
+    const header = document.querySelector('header');  
+    const conteudo = document.querySelector('.conteudo');  
+    const rodape = document.querySelector('.rodape');  
+    const menuItens1 = document.getElementById("menu-itens1");
+    header.style.backgroundColor = 'rgb(47, 147, 180)'; 
+    conteudo.style.backgroundColor = 'rgba(173, 216, 230, 0.5)';
+    rodape.style.backgroundColor = 'rgb(47, 147, 180)'; 
+    menuItens1.style.backgroundColor = 'rgba(173, 216, 230, 0.8)';
+  }
+  
+function trocarCorBotao2() {
+    const botao1 = document.getElementById("botao2");
+    const header = document.querySelector('header');  
+    const conteudo = document.querySelector('.conteudo');  
+    const rodape = document.querySelector('.rodape');  
+    const menuItens1 = document.getElementById("menu-itens1");
+    header.style.backgroundColor = "rgb(104, 214, 163)"; 
+    conteudo.style.backgroundColor = 'rgb(104, 214, 163, 0.5)';
+    rodape.style.backgroundColor = "rgb(104, 214, 163)"; 
+    menuItens1.style.backgroundColor = 'rgb(104, 214, 163, 0.8)';
+  }
+function trocarCorBotao3() {
+    const botao1 = document.getElementById("botao3");
+    const header = document.querySelector('header');  
+    const conteudo = document.querySelector('.conteudo');  
+    const rodape = document.querySelector('.rodape');  
+    const menuItens1 = document.getElementById("menu-itens1");
+    header.style.backgroundColor = "rgb(217, 219, 88)"; 
+    conteudo.style.backgroundColor = 'rgb(217, 219, 88, 0.5)';
+    rodape.style.backgroundColor = "rgb(217, 219, 88)"; 
+    menuItens1.style.backgroundColor = 'rgba(217, 219, 88, 0.8)';
+  }
